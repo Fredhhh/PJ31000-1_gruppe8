621a3f48b48bb339a54b8adc5bec878fdac9f95c
Fredhhh <fred.stangervag@hotmail.com>
2016-10-28 12:02:28 +0200

    Implementert alle scener

---
+++ VR_Showroom/Assets/Scripts/Bad.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bad : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		tp2 ();
+	}
+
+	// Update is called once per frame
+	public void tp2 () {
+		gameObject.transform.position = new Vector3 (6, 0.5f, -5);
+	}
+}
