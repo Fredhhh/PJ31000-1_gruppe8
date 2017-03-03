84e070d67d1c435c91e7c4c72a45955bce90dd2a
Fredhhh <fred.stangervag@hotmail.com>
2017-03-03 16:40:18 +0100

    matlibrary

---
+++ VR_Showroom/Assets/Scripts/MatLibrary.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatLibrary : MonoBehaviour {
+
+    public Material[] kitchenWhites;
+    public Material[] kitchenTiles;
+    public Material[] couch;
+    public Material[] mainWall;
+    public Material[] mainFloor;
+    public Material[] bathroomTilesWall;
+    public Material[] bathroomTilesFloor;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
