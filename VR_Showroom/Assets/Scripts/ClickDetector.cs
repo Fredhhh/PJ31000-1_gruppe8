6e915af8ac81986b60a211d079184da3003207bb
Fredhhh <fred.stangervag@hotmail.com>
2017-03-08 02:02:23 +0100

    material changing

---
+++ VR_Showroom/Assets/Scripts/ClickDetector.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickDetector : MonoBehaviour {
+
+    public bool clicked;
+
+    public bool kitchenWhites;
+    public bool kitchenTiles;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void OnMouseEnter()
+    {
+
+    }
+
+    public void OnMouseDown()
+    {
+        clicked = true;
+
+        if (tag.Equals("KitchenWhites"))
+        {
+            kitchenTiles = false;
+            kitchenWhites = true;
+            transform.tag = "KitchenWhites";
+        }
+
+        else if (tag.Equals("KitchenTiles"))
+        {
+            kitchenWhites = false;
+            kitchenTiles = true;
+            transform.tag = "ChangeableObject";
+
+        }
+
+    }
+    
+
+}
+
--- VR_Showroom/Assets/Scripts/MatLibrary.cs
+++ VR_Showroom/Assets/Scripts/MatLibrary.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MatLibrary : MonoBehaviour {
+
+    public ClickDetector[] clickDetectorScript;
 
     public Material[] kitchenWhites;
     public Material[] kitchenTiles;
@@ -12,12 +15,81 @@ public class MatLibrary : MonoBehaviour {
     public Material[] bathroomTilesWall;
     public Material[] bathroomTilesFloor;
 
+    public RectTransform content;
+    public RectTransform texturePF;
+    Button button;
+    RawImage image;
+    Renderer material;
+
+    GameObject[] changeableMaterial;
+    public int i;
+
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
+    void Start()
+    {
+
+        button = texturePF.GetComponent<Button>();
+        image = texturePF.GetComponent<RawImage>();
+        material = texturePF.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        changeableMaterial = GameObject.FindGameObjectsWithTag("ChangeableMaterial");
+
+
+        for (i = 0; i < clickDetectorScript.Length; i++)
+        {
+            if (clickDetectorScript[i].clicked == true && clickDetectorScript[i].kitchenWhites == true)
+            {
+                DeleteCurrentMat();
+
+                foreach (Material mat in kitchenWhites)
+                {
+                    image.texture = mat.mainTexture;
+                    image.color = mat.color;
+                    material.material = mat;
+                    material.enabled = false;
+
+                    Instantiate(texturePF, content, false);
+                }
+                clickDetectorScript[i].transform.tag = "ChangeableObject";
+            }
+
+            
+
+            else if (clickDetectorScript[i].clicked == true && clickDetectorScript[i].kitchenTiles == true)
+            {
+                DeleteCurrentMat();
+
+                foreach (Material mat in kitchenTiles)
+                {
+                    image.texture = mat.mainTexture;
+                    image.color = mat.color;
+                    material.material = mat;
+                    material.enabled = false;
+
+                    Instantiate(texturePF, content, false);
+                }
+                clickDetectorScript[i].transform.tag = "ChangeableObject";
+            }
+
+           
+            clickDetectorScript[i].clicked = false;
+            
+
+        }
+       
+
+    }
+
+    void DeleteCurrentMat()
+    {
+        foreach (GameObject obj in changeableMaterial)
+        {
+            Destroy(obj);
+        }
+    }
 }
