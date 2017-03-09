bf2f9783c20a795529ef3961542029a7215cd4a9
Fredhhh <fred.stangervag@hotmail.com>
2017-03-09 12:17:54 +0100

    att-tab-upd

--- VR_Showroom/Assets/Scripts/AttachTablet.cs
+++ VR_Showroom/Assets/Scripts/AttachTablet.cs
@@ -6,12 +6,26 @@ using System.Collections.Generic;
 public class AttachTablet : MonoBehaviour
 {
 
-    public GameObject Hand;
+    public GameObject Hand1;
+    public GameObject Hand2;
 
-    void Update()
+    public bool leftHand;
+    public bool rightHand;
+
+    void OnTriggerEnter(Collider other)
     {
-        transform.SetParent(Hand.transform);
-        
-    }
+        if (other.gameObject.tag.Equals("Hand1"))
+        {
+            transform.SetParent(Hand1.transform);
+            leftHand = true;
+            rightHand = false;
+        }
+        else if (other.gameObject.tag.Equals("Hand2"))
+        {
+            transform.SetParent(Hand2.transform);
+            rightHand = true;
+            leftHand = false;
+        }
 
+    }
 }
