0309488cdc202a67bb77f904366944455b941dc8
Fredh <fred.stangervag@hotmail.com>
2017-03-10 11:11:48 +0100

    ddd

--- VR_Showroom/Assets/Scripts/TpScript.cs
+++ VR_Showroom/Assets/Scripts/TpScript.cs
@@ -4,60 +4,62 @@ using UnityEngine;
 
 public class TpScript : MonoBehaviour {
 
+	public GameObject player;
+
     public void Kitchen ()
     {  
-        gameObject.transform.position = GameObject.Find("KitchenSP").transform.position;
+        player.transform.position = GameObject.Find("KitchenSP").transform.position;
     }
     public void LivingRoom ()
     {
-        gameObject.transform.position = GameObject.Find("LivingroomSP").transform.position;
+        player.transform.position = GameObject.Find("LivingroomSP").transform.position;
     }
     public void Bedroom1 ()
     {
-        gameObject.transform.position = GameObject.Find("Bedroom1SP").transform.position;
+        player.transform.position = GameObject.Find("Bedroom1SP").transform.position;
     }
     public void Bedroom2()
     {
-        gameObject.transform.position = GameObject.Find("Bedroom2SP").transform.position;
+        player.transform.position = GameObject.Find("Bedroom2SP").transform.position;
     }
     public void Bedroom3()
     {
-        gameObject.transform.position = GameObject.Find("Bedroom3SP").transform.position;
+        player.transform.position = GameObject.Find("Bedroom3SP").transform.position;
     }
     public void Office()
     {
-        gameObject.transform.position = GameObject.Find("OfficeSP").transform.position;
+        player.transform.position = GameObject.Find("OfficeSP").transform.position;
     }
     public void Washroom()
     {
-        gameObject.transform.position = GameObject.Find("WashroomSP").transform.position;
+        player.transform.position = GameObject.Find("WashroomSP").transform.position;
     }
     public void Balcony1()
     {
-        gameObject.transform.position = GameObject.Find("Balcony1SP").transform.position;
+        player.transform.position = GameObject.Find("Balcony1SP").transform.position;
     }
     public void Balcony2()
     {
-        gameObject.transform.position = GameObject.Find("Balcony2SP").transform.position;
+        player.transform.position = GameObject.Find("Balcony2SP").transform.position;
     }
     public void Entre()
     {
-        gameObject.transform.position = GameObject.Find("EntreSP").transform.position;
+        player.transform.position = GameObject.Find("EntreSP").transform.position;
     }
     public void Hallway1()
     {
-        gameObject.transform.position = GameObject.Find("Hallway1SP").transform.position;
+        player.transform.position = GameObject.Find("Hallway1SP").transform.position;
     }
     public void Hallway2()
     {
-        gameObject.transform.position = GameObject.Find("Hallway2SP").transform.position;
+        player.transform.position = GameObject.Find("Hallway2SP").transform.position;
     }
     public void Bathroom1()
     {
-        gameObject.transform.position = GameObject.Find("Bathroom1SP").transform.position;
+        player.transform.position = GameObject.Find("Bathroom1SP").transform.position;
     }
     public void Bathroom2()
     {
-        gameObject.transform.position = GameObject.Find("Bathroom2SP").transform.position;
+        player.transform.position = GameObject.Find("Bathroom2SP").transform.position;
     }
 }
