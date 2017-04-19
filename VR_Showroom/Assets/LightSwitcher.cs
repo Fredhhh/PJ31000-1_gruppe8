8fa0409a600c6331f0c5f3cbb61da077c1b39190
strawb3rry <vildeba@hotmail.com>
2017-04-19 19:45:22 +0200

    Lagt til lysbrytere og bad

---
+++ VR_Showroom/Assets/LightSwitcher.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSwitcher : MonoBehaviour
+{
+
+
+
+    public GameObject light;
+  
+
+    public bool on;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+        on = false;
+        light.GetComponent<Light>().enabled = false;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+
+    void OnMouseDown()
+    {
+
+        if (on == false)
+        {
+            on = true;
+            light.GetComponent<Light>().enabled = true;
+
+
+        }
+
+        else if (on == true)
+        {
+            on = false;
+            light.GetComponent<Light>().enabled = false;
+
+        }
+    }
+}
